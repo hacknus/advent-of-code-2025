9f457651716c1348943545d640c944d451352d0a
hacknus <linus.stoeckli@unibe.ch>
2025-12-07 15:15:37 +0100

    tree is too slow

--- src/days/day_7.rs
+++ src/days/day_7.rs
@@ -4,6 +4,80 @@ use itertools::Itertools;
 
 pub struct DaySeven {}
 
+struct Node {
+    position: (usize, usize),
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
+    is_leaf: bool,
+}
+
+fn build_tree(
+    current_node: &mut Node,
+    beam_splitters: &Vec<(usize, usize)>,
+    x_max: usize,
+    y_max: usize,
+) {
+    let (x, y) = current_node.position;
+    for y_i in y..y_max {
+        // check left
+        if x > 0 && beam_splitters.contains(&(x - 1, y_i)) && current_node.left.is_none() {
+            let mut left = Node {
+                position: (x - 1, y_i),
+                left: None,
+                right: None,
+                is_leaf: false,
+            };
+            build_tree(&mut left, beam_splitters, x_max, y_max);
+            current_node.left = Some(Box::from(left));
+        }
+        // check right
+        if x < x_max - 1 && beam_splitters.contains(&(x + 1, y_i)) && current_node.right.is_none() {
+            let mut right = Node {
+                position: (x + 1, y_i),
+                left: None,
+                right: None,
+                is_leaf: false,
+            };
+            build_tree(&mut right, beam_splitters, x_max, y_max);
+            current_node.right = Some(Box::from(right));
+        }
+        
+        // check leaves
+        if y_i == y_max - 1 && current_node.right.is_none() {
+            let right = Node {
+                position: (x + 1, y_i),
+                left: None,
+                right: None,
+                is_leaf: true,
+            };
+            current_node.right = Some(Box::from(right));
+        }
+        if y_i == y_max - 1 && current_node.left.is_none() {
+            let left = Node {
+                position: (x - 1, y_i),
+                left: None,
+                right: None,
+                is_leaf: true,
+            };
+            current_node.left = Some(Box::from(left));
+        }
+    }
+}
+
+fn count_leaves(current_node: &Node) -> u32 {
+    if current_node.is_leaf {
+        return 1;
+    }
+    let mut counter = 0;
+    if let Some(left) = &current_node.left {
+        counter += count_leaves(left);
+    }
+    if let Some(right) = &current_node.right {
+        counter += count_leaves(right);
+    }
+    counter 
+}
+
 fn scan_up(
     x: usize,
     y: usize,
@@ -69,9 +143,23 @@ impl Problem for DaySeven {
             }
         }
 
-        let mut counter = 0;
+        let mut root = Node {
+            position: start,
+            left: None,
+            right: None,
+            is_leaf: false,
+        };
+
+        build_tree(
+            &mut root,
+            &beam_splitters,
+            contents[0].len(),
+            contents.len(),
+        );
         
+        println!("built tree!");
         
+        let counter = count_leaves(&root);
 
         format!("{}", counter)
     }
